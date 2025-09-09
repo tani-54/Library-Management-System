59f8f61bef26d2c44372cc2c204e17a751e86935
Tanisha <tanis13861@gmail.com>
2025-09-09 22:04:42 +0530

    fix problem

--- Library-Management-System---PHP-MySql-master/captcha.php
+++ Library-Management-System---PHP-MySql-master/captcha.php
@@ -1,13 +1,27 @@
-<?php 
-session_start(); 
-$text = rand(10000,99999); 
-$_SESSION["vercode"] = $text; 
-$height = 25; 
-$width = 65;   
-$image_p = imagecreate($width, $height); 
-$black = imagecolorallocate($image_p, 0, 0, 0); 
-$white = imagecolorallocate($image_p, 255, 255, 255); 
-$font_size = 14; 
-imagestring($image_p, $font_size, 5, 5, $text, $white); 
-imagejpeg($image_p, null, 80); 
+<?php
+session_start();
+
+// generate random code
+$text = rand(10000, 99999);
+$_SESSION["vercode"] = $text;
+
+$height = 25;
+$width  = 65;
+
+// create image
+$image_p = imagecreate($width, $height);
+
+// colors
+$black = imagecolorallocate($image_p, 0, 0, 0);
+$white = imagecolorallocate($image_p, 255, 255, 255);
+
+// set header for jpeg
+header("Content-Type: image/jpeg");
+
+// draw string (use font 5 for bigger size)
+imagestring($image_p, 5, 8, 5, $text, $white);
+
+// output
+imagejpeg($image_p, null, 80);
+imagedestroy($image_p);
 ?>
